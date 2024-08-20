a4c420ffbb5e8e0a510fa42602f1a3276f74fe85
Larisa Antokhina <laranto1985@gmail.com>
2024-08-20 14:41:19 +0300

    add camera only view, switch only/round view

--- src/webcamera-renderer.ts
+++ src/webcamera-renderer.ts
@@ -2,7 +2,18 @@
   const cameraSelect = document.getElementById(
     "cameraSelect"
   ) as HTMLSelectElement
-  const video = document.getElementById("video") as HTMLVideoElement
+  const webcamRound = document.getElementById(
+    "webcam-round"
+  ) as HTMLVideoElement
+  const webcamSquare = document.getElementById(
+    "webcam-square"
+  ) as HTMLVideoElement
+  const cameraOnlyBtn = document.getElementById(
+    "cameraOnlyBtn"
+  ) as HTMLButtonElement
+  const stopCameraOnlyBtn = document.getElementById(
+    "stopCameraOnlyBtn"
+  ) as HTMLButtonElement
   const startRecordBtn = document.getElementById(
     "startWebcameraRecordBtn"
   ) as HTMLButtonElement
@@ -15,13 +26,17 @@
   let mediaRecorder: MediaRecorder
   let selectedDeviceId
 
+  // move to global state, is Camera only
+  let isCamOnly: Boolean
+
   async function getCameras() {
     const devices = await navigator.mediaDevices.enumerateDevices()
-    const videoDevices = devices.filter(
+
+    const webcamRoundDevices = devices.filter(
       (device) => device.kind === "videoinput"
     )
 
-    videoDevices.forEach((device) => {
+    webcamRoundDevices.forEach((device) => {
       const option = document.createElement("option")
       option.value = device.deviceId
       option.text = device.label || `Camera ${cameraSelect.length + 1}`
@@ -29,14 +44,37 @@
     })
   }
 
+  cameraOnlyBtn.addEventListener("click", () => {
+    if (currentStream) {
+      isCamOnly = true
+
+      switchCameraView(isCamOnly)
+
+      if (selectedDeviceId.length > 0) {
+        startVideo(selectedDeviceId, isCamOnly)
+        startRecordBtn.disabled = false
+      } else {
+        stopVideo()
+        startRecordBtn.disabled = true
+      }
+    } else {
+      console.log("Choose webcamera")
+    }
+  })
+
+  stopCameraOnlyBtn.addEventListener("click", () => {
+    console.log("Cam only stoppped")
+    switchCameraView(!isCamOnly)
+  })
+
   cameraSelect.addEventListener("change", (event) => {
     selectedDeviceId = (event.target as HTMLSelectElement).value
     if (selectedDeviceId.length > 0) {
-      startVideo(selectedDeviceId)
-      startRecordBtn.disabled = false
+      switchCameraView(isCamOnly)
+      startVideo(selectedDeviceId, isCamOnly)
     } else {
       stopVideo()
-      startRecordBtn.disabled = true
+      isCamOnly = false
     }
   })
 
@@ -70,7 +108,19 @@
     }
   })
 
-  function startVideo(deviseId) {
+  function switchCameraView(isCamOnly) {
+    if (isCamOnly) {
+      webcamRound.classList.add("hidden")
+      webcamSquare.classList.remove("hidden")
+      webcamSquare.srcObject = currentStream
+    } else {
+      webcamRound.classList.remove("hidden")
+      webcamSquare.classList.add("hidden")
+      webcamRound.srcObject = currentStream
+    }
+  }
+
+  function startVideo(deviseId, isCamOnly) {
     const constraints = {
       video: { deviceId: { exact: deviseId } },
     }
@@ -80,7 +130,7 @@
     media
       .then((stream) => {
         currentStream = stream
-        video.srcObject = stream
+        switchCameraView(isCamOnly)
       })
       .catch((e) => console.log(e))
   }
@@ -116,7 +166,10 @@
   function stopVideo() {
     const tracks = currentStream.getTracks()
     tracks.forEach((track) => track.stop())
-    video.srcObject = null
+    webcamRound.classList.add("hidden")
+    webcamRound.srcObject = null
+    webcamSquare.classList.add("hidden")
+    webcamSquare.srcObject = null
     currentStream = null
   }
 
