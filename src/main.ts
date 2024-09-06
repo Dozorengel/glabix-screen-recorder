18d01134dce4de54f3cca5e8db2b80ac4f69d74e
danzel123 <mrfisher123@mail.ru>
2024-09-06 17:33:13 +0800

    add glabix file link

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import {
   protocol,
   dialog,
   nativeTheme,
+  shell,
 } from "electron"
 import path from "path"
 import os from "os"
@@ -170,8 +171,7 @@ function checkUnprocessedFiles() {
     } else {
       setLog(`No next chunk`, false)
     }
-  }
-  {
+  } else {
     setLog(`No unprocessed files!`, false)
   }
 }
@@ -490,6 +490,14 @@ ipcMain.on(FileUploadEvents.FILE_CREATED_ON_SERVER, (event) => {
   chunkStorage.addStorage(chunks, uuid).then(() => {
     checkUnprocessedFiles()
   })
+  const shared =
+    import.meta.env.VITE_AUTH_APP_URL +
+    "recorder/org/" +
+    tokenStorage.organizationId +
+    "/" +
+    "library/" +
+    uuid
+  shell.openExternal(shared)
 })
 
 ipcMain.on(FileUploadEvents.LOAD_FILE_CHUNK, (event) => {
